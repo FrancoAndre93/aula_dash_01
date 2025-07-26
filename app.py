8191f8f7a3103f0c5958b8f179e06a3ac7de79f6
Franco Silva <franco@email.com>
2025-07-26 09:37:57 -0300

    v2

--- app.py
+++ app.py
@@ -1,20 +1,36 @@
 import dash
-from dash import html, dcc
+from dash import dcc, html, Input, Output
 import plotly.express as px
 import pandas as pd
 
 df = pd.DataFrame({
-    "Mês":['Jan', 'Fev', 'Mar', 'Abr', 'Mai'],
-    "Vendas": [100,120,50, 200, 180]
+    "Cidade": ["São Paulo", "Rio", "Fortaleza", "São Paulo", "Rio", "Fortaleza"],
+    "Mês": ["Jan", "Jan", "Jan", "Fev", "Fev", "Fev"],
+    "Vendas": [100, 80, 70, 120, 90, 1000]
 })
 
-fig = px.line(df, x='Mês', y='Vendas')
 app = dash.Dash(__name__)
+
 app.layout = html.Div([
-    html.H1("Dashboard de vendas", 
-            style={'textAlign':'center'}),
-    dcc.Graph(figure=fig)
+    html.H1("Dashboard Interativo de Vendas"),
+
+    html.Label("Escolha a cidade:"),
+    dcc.Dropdown(
+        id="dropdown-cidade",
+        options=[{"label": c, "value": c} for c in df["Cidade"].unique()],
+        value="São Paulo"
+    ),
+
+    dcc.Graph(id="grafico-vendas")
 ])
 
-if __name__ == "__main__":
-  app.run()
+@app.callback(
+    Output("grafico-vendas", "figure"),
+    Input("dropdown-cidade", "value")
+)
+def atualizar_grafico(cidade_selecionada):
+    df_filtrado = df[df["Cidade"] == cidade_selecionada]
+    fig = px.bar(df_filtrado, x="Mês", y="Vendas", title=f"Vendas em {cidade_selecionada}")
+    return fig
+
+app.run(port=8051)
